9455ef6415513835b0e94893b2d3c512092e4aba
chenpian <1837890880@qq.com>
2016-11-02 17:44:44 +0800

    init

---
+++ src/main/java/com/keeper/pdf/PdfTest.java
@@ -0,0 +1,46 @@
+package com.keeper.pdf;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.tool.xml.XMLWorkerHelper;
+
+public class PdfTest {
+	public static final String HTML = "D:/1.html";
+    public static final String DEST = "D:/1.pdf";
+ 
+    /**
+     * Creates a PDF with the words "Hello World"
+     * @param file
+     * @throws IOException
+     * @throws DocumentException
+     */
+    public void createPdf(String file) throws IOException, DocumentException {
+        // step 1
+        Document document = new Document();
+        // step 2
+        PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(file));
+        // step 3
+        document.open();
+        // step 4
+        XMLWorkerHelper.getInstance().parseXHtml(writer, document,
+                new FileInputStream(HTML), Charset.forName("UTF-8"));
+        // step 5
+        document.close();
+    }
+ 
+    /**
+     * Main method
+     */
+    public static void main(String[] args) throws IOException, DocumentException {
+        File file = new File(DEST);
+        file.getParentFile().mkdirs();
+        new PdfTest().createPdf(DEST);
+    }
+}
